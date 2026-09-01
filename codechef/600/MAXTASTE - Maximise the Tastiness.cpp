ef4d97a6e5c8fd7e5b0762d7d72d84ce89c12b73
Krishna Paliya <krishna.25b01011203@abes.ac.in>
2026-09-01 22:31:53 +0530

    Add Maximise the Tastiness (600)

---
+++ codechef/600/MAXTASTE - Maximise the Tastiness.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int a, b, c, d;
+        cin >> a >> b >> c >> d;
+
+        cout << max(a, b) + max(c, d) << endl;
+    }
+
+    return 0;
+}
