4c00a33e71aec13fdcd1f2e42be1885955bd5f16
Krishna Paliya <krishna.25b01011203@abes.ac.in>
2026-09-03 09:32:01 +0530

    Add Finding Square Roots (600)

---
+++ codechef/600/FSQRT - Finding Square Roots.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int m,n;
+    cin>>m;
+    while(m--) {
+        cin>>n;
+        int X= sqrt(n);
+        cout<<X<<endl;
+    }
+
+}
