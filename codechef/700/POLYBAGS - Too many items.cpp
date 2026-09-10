a399fc8fc144764601a5bbcfa45eeaef44ad9010
Krishna Paliya <krishna.25b01011203@abes.ac.in>
2026-09-10 21:45:53 +0530

    Add Too many items (700)

---
+++ codechef/700/POLYBAGS - Too many items.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int N;
+        cin >> N;
+
+        cout << (N + 9) / 10 << endl;
+    }
+
+    return 0;
+}
