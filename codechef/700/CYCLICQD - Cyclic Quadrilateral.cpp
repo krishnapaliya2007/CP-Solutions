e374bf91701a7e979fa2e16dc4b990a4ea07b826
Krishna Paliya <krishna.25b01011203@abes.ac.in>
2026-09-10 21:45:24 +0530

    Add Cyclic Quadrilateral (700)

---
+++ codechef/700/CYCLICQD - Cyclic Quadrilateral.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int A, B, C, D;
+        cin >> A >> B >> C >> D;
+
+        if (A + C == 180)
+            cout << "YES" << endl;
+        else
+            cout << "NO" << endl;
+    }
+
+    return 0;
+}
