fc2a835a8bf93919f72591ebb90c03accd6e0389
Krishna Paliya <krishna.25b01011203@abes.ac.in>
2026-09-14 22:01:29 +0530

    Add Small Factorial (700)

---
+++ codechef/700/FLOW018 - Small Factorial.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int N;
+        cin >> N;
+
+        long long fact = 1;
+
+        for (int i = 1; i <= N; i++) {
+            fact = fact * i;
+        }
+
+        cout << fact << endl;
+    }
+
+    return 0;
+}
