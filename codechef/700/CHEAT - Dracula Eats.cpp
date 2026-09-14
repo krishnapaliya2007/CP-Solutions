55e2461bdc757d7125c1995a790effbf3ff892cd
Krishna Paliya <krishna.25b01011203@abes.ac.in>
2026-09-14 22:06:02 +0530

    Add Dracula Eats (700)

---
+++ codechef/700/CHEAT - Dracula Eats.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int N;
+        cin >> N;
+
+        cout << (N + 5) / 7 << endl;
+    }
+
+    return 0;
+}
