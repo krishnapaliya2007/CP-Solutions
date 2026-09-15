6aa2cb0e6868232d35c6a8552c071c3f7d3c0bda
Krishna Paliya <krishna.25b01011203@abes.ac.in>
2026-09-15 18:12:41 +0530

    Add Mutated Minions (700)

---
+++ codechef/700/CHN15A - Mutated Minions.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int N, K;
+        cin >> N >> K;
+
+        int count = 0;
+
+        for (int i = 0; i < N; i++) {
+            int x;
+            cin >> x;
+
+            if ((x + K) % 7 == 0)
+                count++;
+        }
+
+        cout << count << endl;
+    }
+
+    return 0;
+}
