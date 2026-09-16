68f07daf478767e433aca6db31d23ce69321f032
Krishna Paliya <krishna.25b01011203@abes.ac.in>
2026-09-16 20:11:02 +0530

    Add Bus Seating (Unrated)

---
+++ codechef/Unrated/BUSSEAT - Bus Seating.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int N, K;
+        cin >> N >> K;
+
+        if (K <= N)
+            cout << 0 << endl;
+        else
+            cout << 2 * (K - N) << endl;
+    }
+
+    return 0;
+}
