3b4cc41e68b70813ad12a413a09ea3a5844d76f8
Krishna Paliya <krishna.25b01011203@abes.ac.in>
2026-09-16 11:02:00 +0530

    Add Get Lowest Free (700)

---
+++ codechef/700/SALE - Get Lowest Free.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int A, B, C;
+        cin >> A >> B >> C;
+
+        cout << A + B + C - min({A, B, C}) << endl;
+    }
+
+    return 0;
+}
