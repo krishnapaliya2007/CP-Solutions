67704d06663b3c847f83433c4549f333843b1903
Krishna Paliya <krishna.25b01011203@abes.ac.in>
2026-09-17 20:02:51 +0530

    Add Sasta Shark Tank (500)

---
+++ codechef/500/SST - Sasta Shark Tank.cpp
@@ -0,0 +1,22 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int A, B;
+        cin >> A >> B;
+
+        if (2 * A > B)
+            cout << "FIRST" << endl;
+        else if (2 * A < B)
+            cout << "SECOND" << endl;
+        else
+            cout << "ANY" << endl;
+    }
+
+    return 0;
+}
