98e96eff55b4549b7012f6ef7e439139148a0598
Krishna Paliya <krishna.25b01011203@abes.ac.in>
2026-09-17 20:01:01 +0530

    Add The Cooler Dilemma 2 (700)

---
+++ codechef/700/WATERCOOLER2 - The Cooler Dilemma 2.cpp
@@ -0,0 +1,17 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        long long X, Y;
+        cin >> X >> Y;
+
+        cout << (Y - 1) / X << endl;
+    }
+
+    return 0;
+}
