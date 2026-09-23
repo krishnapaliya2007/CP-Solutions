caf4ba9a3e957b52f457d648eb3dbff7975f8bb5
Krishna Paliya <krishna.25b01011203@abes.ac.in>
2026-09-23 20:51:46 +0530

    Add Pigeon Attack (Unrated)

---
+++ codechef/Unrated/PGNATK - Pigeon Attack.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int N, K;
+        cin >> N >> K;
+
+        int ans = N + (N - 1) / (K - 1);
+
+        cout << ans << endl;
+    }
+
+    return 0;
+}
